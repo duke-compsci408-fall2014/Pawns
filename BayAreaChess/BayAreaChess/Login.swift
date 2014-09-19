38c4965b621ffc917c235e9f584f404ef2b7ad89
CarlosReyesStoneham <cer26@duke.edu>
2014-09-18 21:03:55 -0400

    added basic login functionality, login storyboard, login segue, and gitignore

---
+++ BayAreaChess/BayAreaChess/Login.swift
@@ -0,0 +1,41 @@
+//
+//  Login.swift
+//  BayAreaChess
+//
+//  Created by Carlos Reyes on 9/18/14.
+//  Copyright (c) 2014 Bay Area Chess. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+	
+	@IBOutlet var username : UITextField!
+	@IBOutlet var password : UITextField!
+	@IBOutlet var label : UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	/* 
+		This is where we'll normally be making database requests.
+		It triggers the segue to transition back to the homepage
+		if the creditials are accepted. 
+	*/
+	@IBAction func verifyLogin (sender : AnyObject) {
+		if (username.text == "Pawns" && password.text == "Pawns") {
+			self.performSegueWithIdentifier("login", sender: sender as UIButton);
+		}
+		else {
+			label.textColor = UIColor.redColor();
+			label.text = "Rejected!";
+		}
+	}
+}
