65fc400c455e93f3c15f74f88ecf3eb368ce9b90
czarlos <cer26@duke.edu>
2014-10-04 02:36:19 -0400

    updated server

--- api_server/server.js
+++ api_server/server.js
@@ -4,9 +4,10 @@ var express = require('express'),
     connectionpool = mysql.createPool({
         host     : 'localhost',
         user     : 'root',
-        password : process.env.BAC_PASS,
-        database : 'bachess'
+        password : process.env.BACKUP,
+        database : 'backup'
     });
+/*
 app.get('/:table', function(req,res){
     connectionpool.getConnection(function(err, connection) {
         if (err) {
@@ -42,6 +43,83 @@ app.get('/:table', function(req,res){
         }
     });
 });
+*/
+
+app.get('/login/:user', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            var query = 'SELECT * FROM auth_user WHERE username=' + "\"" + req.params.user + "\"";
+            console.log(query);
+            connection.query(query, req.params.id, function(err, rows, fields) {
+
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                else {
+                    //console.log(rows.length);
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        fields: fields,
+                        json:   rows,
+                        length: rows.length
+                    });
+                }
+                connection.release();
+            });
+        }
+    });
+});
+
+app.get('/tournaments', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            var query = 'SELECT name,description,start_date FROM tournament_tournaments WHERE start_date > now()';
+            console.log(query);
+            connection.query(query, req.params.id, function(err, rows, fields) {
+
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                else {
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        fields: fields,
+                        json:   rows,
+                        length: rows.length
+                    });
+                }
+                connection.release();
+            });
+        }
+    });
+});
+
 app.get('/:table/:id', function(req,res){});
 app.post('/:table', function(req,res){});
 app.put('/:table/:id', function(req,res){});
