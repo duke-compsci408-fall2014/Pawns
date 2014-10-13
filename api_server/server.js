1f0353af9315d6e48c3c8a1503537bd8c4753bba
czarlos <cer26@duke.edu>
2014-10-13 16:24:00 -0400

    added route paths in server, cleared old login endpoint

--- api_server/server.js
+++ api_server/server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     tournaments = require('./routes/tournaments'),
+    login = require('./routes/login'),
+    users = require('./routes/users'),
     app     = express(),
     mysql   = require('mysql'),
     connectionpool = mysql.createPool({
@@ -10,51 +12,16 @@ var express = require('express'),
     });
 
 app.use('/tournaments', tournaments);
-app.use('/login');
-app.use('/users');
+app.use('/login', login);
+app.use('/users', users);
 
-app.get('/login/:user', function(req,res){
-    connectionpool.getConnection(function(err, connection) {
-        if (err) {
-            console.error('CONNECTION error: ',err);
-            res.statusCode = 503;
-            res.send({
-                result: 'error',
-                err:    err.code
-            });
-        } else {
-            var query = 'SELECT * FROM auth_user WHERE username=' + "\"" + req.params.user + "\"";
-            console.log(query);
-            connection.query(query, req.params.id, function(err, rows, fields) {
-
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                else {
-                    //console.log(rows.length);
-                    res.send({
-                        result: 'success',
-                        err:    '',
-                        fields: fields,
-                        json:   rows,
-                        length: rows.length
-                    });
-                }
-                connection.release();
-            });
-        }
-    });
-});
-
+/*
 app.get('/:table/:id', function(req,res){});
 app.post('/:table', function(req,res){});
 app.put('/:table/:id', function(req,res){});
 app.delete('/:table/:id', function(req,res){});
+*/
+
 app.listen(3000);
 console.log('Rest Server Listening on port 3000');
 module.exports = app;
