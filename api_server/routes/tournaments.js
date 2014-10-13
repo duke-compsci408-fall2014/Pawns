5635a4367e75440c87e64219baf4705b641ea35e
czarlos <cer26@duke.edu>
2014-10-13 02:44:14 -0400

    added basic user info rest endpoint

--- api_server/routes/tournaments.js
+++ api_server/routes/tournaments.js
@@ -12,7 +12,8 @@ var connectionpool = mysql.createPool({
 
 
 router.get('/base', function(req,res){
-    var query = 'SELECT name,description,start_date FROM tournament_tournaments WHERE start_date > now() ORDER BY start_date ASC';
+    var query = 'SELECT name,description,start_date
+                FROM tournament_tournaments WHERE start_date > now() ORDER BY start_date ASC';
     utils.runQuery(connectionpool, query, req, res, doStuff);
 });
 
@@ -21,47 +22,9 @@ function doStuff(res) {
 }
 
 router.get('/base/:id', function(req, res) {
-    var query = 'SELECT name, description, amount, cash_prize, discount, status, start_date, end_date FROM tournament_tournaments WHERE id=' + req.params.id;
+    var query = 'SELECT name, description, amount, cash_prize, discount, status,
+                start_date, end_date FROM tournament_tournaments WHERE id=' + req.params.id;
     utils.runQuery(connectionpool, query, req, res, doStuff);
 });
-
-
-var runQuery = function (connectionPool, sqlQuery, req, res, callback) {
-    connectionPool.getConnection(function(err, connection) {
-        if (err) {
-            console.error('CONNECTION error: ',err);
-            res.statusCode = 503;
-            res.send({
-                result: 'error',
-                err:    err.code
-            });
-        } else {
-            var query = sqlQuery; //'SELECT name,description,start_date FROM tournament_tournaments WHERE start_date > now()';
-            console.log(query);
-            connection.query(query, req.params.id, function(err, rows, fields) {
-
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                else {
-                    callback({json: rows});
-                    res.send({
-                        result: 'success',
-                        err:    '',
-                        fields: fields,
-                        json:   rows,
-                        length: rows.length
-                    });
-                }
-                connection.release();
-            });
-        }
-    });
-}
 
 module.exports = router;
