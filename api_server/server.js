2f9c89e29b47b53826e22523ed01cfedfab22eb4
czarlos <cer26@duke.edu>
2014-10-13 02:23:55 -0400

    cleaned out server base

--- api_server/server.js
+++ api_server/server.js
@@ -11,45 +11,6 @@ var express = require('express'),
 
 app.use('/tournaments', tournaments);
 
-
-/*
-app.get('/:table', function(req,res){
-    connectionpool.getConnection(function(err, connection) {
-        if (err) {
-            console.error('CONNECTION error: ',err);
-            res.statusCode = 503;
-            res.send({
-                result: 'error',
-                err:    err.code
-            });
-        } else {
-            connection.query('SELECT * FROM '+req.params.table, req.params.id, function(err, rows, fields) {
-                //console.log(rows);
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.send({
-                        result: 'error',
-                        err:    err.code
-                    });
-                }
-                else {
-                    //console.log(rows.length);
-                    res.send({
-                        result: 'success',
-                        err:    '',
-                        fields: fields,
-                        json:   rows,
-                        length: rows.length
-                    });
-                }
-                connection.release();
-            });
-        }
-    });
-});
-*/
-
 app.get('/login/:user', function(req,res){
     connectionpool.getConnection(function(err, connection) {
         if (err) {
