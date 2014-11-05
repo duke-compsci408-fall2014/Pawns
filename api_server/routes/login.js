861fdeec042934da83e7efd023ef33b70d20c63a
czarlos <cer26@duke.edu>
2014-11-05 06:39:36 +0000

    generating password hashes and preliminary insert query for reg

--- api_server/routes/login.js
+++ api_server/routes/login.js
@@ -4,6 +4,7 @@ var mysql = require('mysql');
 var utils = require('../utils');
 var pbkdf2 = require('pbkdf2-sha256');
 var querystring = require('querystring');
+var crypto = require('crypto');
 
 var connectionpool = mysql.createPool({
         host     : 'localhost',
@@ -21,6 +22,11 @@ var userViewQuery = 'SELECT auth_user.username, auth_user.first_name, auth_user.
                     INNER JOIN player_accounts_playerprofile ON auth_user.id=player_accounts_playerprofile .user_id \
                     WHERE auth_user.username=';
 
+var insertQuery = 'INSERT INTO auth_user (username, first_name, \
+                    last_name, email, password, is_staff, is_active, \
+                    is_superuser, last_login, date_joined) \
+                    VALUES ("czarlos", "Carlos", "Reyes", "cer26@duke.edu", \
+                    "money", 1, 1, 1, NOW(), NOW());'
 
 router.get('/verify/:user/:pass', function (req, res) {
     var query = 'SELECT username, password FROM auth_user WHERE username=' + '\"' + req.params.user + "\"";
@@ -59,6 +65,15 @@ router.post('/update/:user/:fields', function (req, res) {
     }
 });
 
+router.post('/register/:fields', function (req, res) {
+    var fields = req.params.fields;
+    var parsed = querystring.parse(fields);
+    var rawPass = parsed.password.replace(/"/g, '');
+    var salt = crypto.randomBytes(8).toString('base64');
+    var hashedPass = 'pbkdf2_sha256$10000$' + salt + '$' + pbkdf2(rawPass, new Buffer(salt), 10000, 32).toString('base64');
+    console.log(hashedPass);
+});
+
 function validate (json, res, req) {
     if (validatePassword(req.params.pass, json.json[0].password)) {
         res.send({verification: 'success'});
@@ -79,10 +94,10 @@ function postInfo (json, res, req) {
 }
 
 var validatePassword = function (key, string) {
-        var parts = string.split('$');
-        var iterations = parts[1];
-        var salt = parts[2];
-        return pbkdf2(key, new Buffer(salt), iterations, 32).toString('base64') === parts[3];
+    var parts = string.split('$');
+    var iterations = parts[1];
+    var salt = parts[2];
+    return pbkdf2(key, new Buffer(salt), iterations, 32).toString('base64') === parts[3];
 };
 
 module.exports = router;
